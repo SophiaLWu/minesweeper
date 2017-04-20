cac9fa9cd9e57c455cb43a9b98f66e9713324fa5
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-04-19 17:23:51 -0700

    Add mines and hints to board

--- js/main.js
+++ js/main.js
@@ -6,9 +6,14 @@ $(document).ready(function() {
 var board = {
   board: [],
   size: 9,
+  mineCount: 0,
+  mineRatio: 0.4,
   init: function() {
     this.board = [];
+    this.mineCount = Math.floor(this.size * this.size * this.mineRatio);
     this.newBoard();
+    this.addMines();
+    this.addHints();
     this.renderBoard();
   },
   newBoard: function() {
@@ -18,6 +23,40 @@ var board = {
         this.board[i].push("");
       }
     }
+  },
+  addMines: function() {
+    for (var i = 0; i < this.mineCount; i++) {
+      var randomRow = Math.floor(Math.random() * (this.size));
+      var randomCol = Math.floor(Math.random() * (this.size));
+      while (this.board[randomRow][randomCol] == "mine") {
+        randomRow = Math.floor(Math.random() * (this.size));
+        randomCol = Math.floor(Math.random() * (this.size));
+      }
+      this.board[randomRow][randomCol] = "mine";
+    }
+  },
+  addHints: function() {
+    for (var i = 0; i < this.size; i++) {
+      for (var j = 0; j < this.size; j++) {
+        if (this.board[i][j] != "mine") {
+          var count = 0;
+          if (this.isMine(i - 1, j - 1)) count += 1;
+          if (this.isMine(i - 1, j)) count += 1;
+          if (this.isMine(i - 1, j + 1)) count += 1;
+          if (this.isMine(i, j - 1)) count += 1;
+          if (this.isMine(i, j + 1)) count += 1;
+          if (this.isMine(i + 1, j - 1)) count += 1;
+          if (this.isMine(i + 1, j)) count += 1;
+          if (this.isMine(i + 1, j + 1)) count += 1;
+          this.board[i][j] = count;
+        }
+      }
+    }
+  },
+  isMine: function(row, col) {
+    if (row < 0 || row >= this.size || col < 0 || col >= this.size) return;
+    return this.board[row][col] != "undefined" &&
+           this.board[row][col] === "mine";
   },
   renderBoard: function() {
     $(".board-container").append("<table class='board'></table>");
