a1dd7b6844f06dadbb0b99d0194deef0e6599bb4
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-04-19 18:25:19 -0700

    Complete logic to update board when user clicks on a square

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 
 $(document).ready(function() {
-  board.init();
+  game.init();
 });
 
 var board = {
   board: [],
   size: 9,
   mineCount: 0,
-  mineRatio: 0.4,
+  mineRatio: 0.3,
   init: function() {
     this.board = [];
     this.mineCount = Math.floor(this.size * this.size * this.mineRatio);
@@ -28,17 +28,17 @@ var board = {
     for (var i = 0; i < this.mineCount; i++) {
       var randomRow = Math.floor(Math.random() * (this.size));
       var randomCol = Math.floor(Math.random() * (this.size));
-      while (this.board[randomRow][randomCol] == "mine") {
+      while (this.board[randomRow][randomCol] == "M") {
         randomRow = Math.floor(Math.random() * (this.size));
         randomCol = Math.floor(Math.random() * (this.size));
       }
-      this.board[randomRow][randomCol] = "mine";
+      this.board[randomRow][randomCol] = "M";
     }
   },
   addHints: function() {
     for (var i = 0; i < this.size; i++) {
       for (var j = 0; j < this.size; j++) {
-        if (this.board[i][j] != "mine") {
+        if (this.board[i][j] != "M") {
           var count = 0;
           if (this.isMine(i - 1, j - 1)) count += 1;
           if (this.isMine(i - 1, j)) count += 1;
@@ -56,17 +56,64 @@ var board = {
   isMine: function(row, col) {
     if (row < 0 || row >= this.size || col < 0 || col >= this.size) return;
     return this.board[row][col] != "undefined" &&
-           this.board[row][col] === "mine";
+           this.board[row][col] === "M";
   },
   renderBoard: function() {
     $(".board-container").append("<table class='board'></table>");
     for (var i = 0; i < this.size; i++) {
       $(".board").append("<tr class='board-row-" + i + "'></tr>");
       for (var j = 0; j < this.size; j++) {
-        $(".board-row-" + i).append("<td class='board-space' data-row='" + i + 
-                                    "' data-col='" + j + "'>" + 
-                                    this.board[i][j] + "</td>")
+        $(".board-row-" + i).append("<td class='board-square' data-row='" + i + 
+                                    "' data-col='" + j + "'><p>" + 
+                                    this.board[i][j] + "</p></td>")
       }
     }
+  },
+  revealSquare: function($square) {
+    var row = $square.data("row");
+    var col = $square.data("col");
+    var value = board.board[row][col];
+    if (value === "M") {
+      $square.find("p").show()
+      $square.addClass("revealed");
+    } else if (value === 0) {
+      $square.find("p").text("").show();
+      $square.addClass("revealed");
+      this.showHint(row - 1, col - 1);
+      this.showHint(row - 1, col);
+      this.showHint(row - 1, col + 1);
+      this.showHint(row, col - 1);
+      this.showHint(row, col + 1);
+      this.showHint(row + 1, col - 1);
+      this.showHint(row + 1, col);
+      this.showHint(row + 1, col + 1);
+    } else {
+      $square.find("p").show();
+      $square.addClass("revealed");
+    }
+  },
+  showHint: function(row, col) {
+    if (row < 0 || row >= this.size || col < 0 || col >= this.size) return;
+    var $square = $(".board-square[data-r ow='" + row + "'][data-col='" + col + "']");
+    if ($square.text() != "M" && !$square.hasClass("revealed")) {
+      if ($square.text() === "0") {
+        board.revealSquare($square);
+      } else {
+        $square.find("p").show();
+        $square.addClass("revealed");
+      }
+    }
+  },
+};
+
+var game = {
+  init: function() {
+    board.init();
+    this.clickSquare();
+  },
+  clickSquare: function() {
+    $(".board-square").on("click", function() {
+      board.revealSquare($(this));
+    });
   },
 };
